750a4e08c6619a126e18e8d60c61bc1e8cd3eddc
Victor Sanchez <incomingvrs@gmail.com>
2019-12-07 12:39:11 -0800

    Removed test code from views.py and template placeholders for homepage

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render
-from tests import heremaps_data
-import suntime_adjust
 
-# Pulls data from data structure to template
-def reminder_time(request):
-    sunrise = heremaps_data["astronomy"]["astronomy"][0]["sunrise"]
-    sunset = heremaps_data["astronomy"]["astronomy"][0]["sunset"]
-    print("Sunrise - - - - - ", sunrise)
-    print("Sunset - - - - - ", sunset)
-
-    context = {
-        'sunrise': sunrise,
-        'sunset': sunset,
-        'sun_time': 'COMING SOON',  # results from suntime_adjust
-    }
-
-    return render(request, 'pages/home.html', context)
-
-
-# Two example views. Change or delete as necessary.
 def home(request):
     context = {
-        'example_context_variable': 'Change me.',
-        'sun_time': 'COMING SOON'   # use here maps reference for times
+
     }
 
     return render(request, 'pages/home.html', context)
